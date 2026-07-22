77731ebc600947340035916c33119611841a2a95
ST10442123 <echoinfluence01@gmail.com>
2026-07-22 09:50:27 +0200

    demo site

---
+++ js/script.js
@@ -0,0 +1,16 @@
+window.addEventListener("scroll", function () {
+
+const nav = document.querySelector("nav");
+
+if(window.scrollY > 50){
+
+nav.style.background = "#ffffff";
+
+}
+else{
+
+nav.style.background="#ffffff";
+
+}
+
+});
